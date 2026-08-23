943223f90733fd1185f00b5e926423239530e25f
tfshorifulislam <tfshorifulislam@gmail.com>
2026-08-23 22:47:06 +0600

    createPost route create done

---
+++ src/services/upload.media.ts
@@ -0,0 +1,28 @@
+import { CreatePostResponse } from "../../types/createPost";
+
+const baseUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
+
+export const createPost = async (formData: FormData): Promise<CreatePostResponse> => {
+
+    if (!baseUrl) {
+        throw new Error("Backend URL is not configured");
+    }
+
+    const response = await fetch(`${baseUrl}/api/create/posts`,
+        {
+            method: "POST",
+            credentials: "include",
+            body: formData,
+        }
+    );
+
+    const result: CreatePostResponse = await response.json();
+
+    if (!response.ok) {
+        throw new Error(
+            result.message || "Failed to create post"
+        );
+    }
+
+    return result;
+};
---
+++ types/createPost.ts
@@ -0,0 +1,24 @@
+export interface PostUser {
+  id: string;
+  name: string;
+  userName: string;
+  image: string | null;
+}
+
+export interface Post {
+  id: string;
+  title: string | null;
+  content: string;
+  mediaUrl: string | null;
+  mediaType: "image" | "video" | null;
+  createdAt: string;
+  updatedAt: string;
+  userId: string;
+  user: PostUser;
+}
+
+export interface CreatePostResponse {
+  success: boolean;
+  message: string;
+  post: Post;
+}
